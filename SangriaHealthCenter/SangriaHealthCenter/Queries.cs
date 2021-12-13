b5ffb56d586986a8eb1bd883e65a64cf7cf16441
mrsaibaandreas <57641877+mrsaibaandreas@users.noreply.github.com>
2021-12-13 18:12:06 +0200

    Update Queries.cs

--- SangriaHealthCenter/SangriaHealthCenter/Queries.cs
+++ SangriaHealthCenter/SangriaHealthCenter/Queries.cs
@@ -9,8 +9,8 @@ namespace SangriaHealthCenter
     class Queries
     {
         const String CreatePatientsTable = @"CREATE TABLE IF NOT EXISTS `Patients`( 
-    v_id INT(6) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    v_group VARCHAR(3) NOT NULL CHECK(v_group IN('0-', '0+', 'A-', 'A+', 'B-', 'B+', 'AB-', 'AB+')),
+    p_id INT(6) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    p_group VARCHAR(3) NOT NULL CHECK(v_group IN('0-', '0+', 'A-', 'A+', 'B-', 'B+', 'AB-', 'AB+')),
     emergency BOOLEAN NOT NULL DEFAULT FALSE,
     name VARCHAR(20) NOT NULL,
     surname VARCHAR(20) NOT NULL,
