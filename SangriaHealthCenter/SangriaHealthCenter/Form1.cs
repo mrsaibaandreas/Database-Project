7d638284d7caff841e581fd8a93d4a216b34264f
mrsaibaandreas <andreasqp@gmail.com>
2021-12-18 19:34:19 +0200

    Added example for retrieving data from DB

--- SangriaHealthCenter/SangriaHealthCenter/Form1.cs
+++ SangriaHealthCenter/SangriaHealthCenter/Form1.cs
@@ -14,21 +14,27 @@ namespace SangriaHealthCenter
     public partial class Form1 : Form
     {
         public Form1()
-        {
+       {
             InitializeComponent();
+          
+
         }
+
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
+        static string connetionString = "server=localhost;database=db_test;uid=root;pwd=example;";
+
+        static MySqlConnection cnn = new MySqlConnection(connetionString);
 
         private void connect_Click(object sender, EventArgs e)
         {
-            string connetionString = null;
+           /* string connetionString = null;
             MySqlConnection cnn;
             connetionString = "server=localhost;database=db_test;uid=root;pwd=example;";
-            cnn = new MySqlConnection(connetionString);
+            cnn = new MySqlConnection(connetionString);*/
             try
             {
                 cnn.Open();
@@ -43,10 +49,27 @@ namespace SangriaHealthCenter
                 createFKs = new MySqlCommand(Q.InitFKs(), cnn);
                 populateTables = new MySqlCommand(Q.populateTables(), cnn);
 
+
+
+
+
                 createTables.ExecuteNonQuery();
                 createFKs.ExecuteNonQuery();
                 populateTables.ExecuteNonQuery();
 
+
+                MySqlCommand selectCmd = new MySqlCommand();
+                MySqlDataReader rdr = null;
+
+                selectCmd.Connection = cnn;
+                selectCmd.CommandText = "SELECT * FROM Patients";
+
+                rdr = selectCmd.ExecuteReader();
+
+                while(rdr.Read())
+                {
+                    Console.WriteLine("Name: "+ rdr["name"].ToString());
+                }
                 //dropTables.ExecuteNonQuery();   //DELETING TABLES FOR TESTING PURPUOSES ONLY
                 cnn.Close();
             }
